bcca44741b9e22cdb71e6594c4ebd4afb4a4e1a2
limozaza <zakboite@gmail.com>
2017-06-13 10:05:36 +0200

    get pseudo

--- src/components/Connexion.js
+++ src/components/Connexion.js
@@ -1,11 +1,21 @@
 import React from 'react';
 
 class Connexion extends React.Component{
+  goToChat = (e) => {
+    e.preventDefault();
+    console.log(this.pseudoInput.value);
+  };
+
   render(){
     return(
       <div className="connexionBox">
-        <form className="connexion">
-          <input type="text" placeholder="Pseudo" required/>
+        <form className="connexion" onSubmit={(e)=> this.goToChat(e)}>
+          <input
+            type="text"
+            placeholder="Pseudo"
+            required
+            ref={(input) => {this.pseudoInput = input}}
+            />
           <button type="submit">GO</button>
         </form>
       </div>
